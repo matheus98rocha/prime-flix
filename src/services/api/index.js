30dcac55d304edcb3f35635bf7770653fe461b92
matheus98rocha <matheus98rocha@gmail.com>
2023-04-02 18:57:27 -0300

    Feat - Login with the api

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -6,4 +6,24 @@ export const userServices = {
       .get("user/641b03b286833afa1c4e3d67")
       .then((e) => e.data);
   },
+  createUser: async (name, userName, password, email) => {
+    return await server
+      .post("user/", {
+        name: "Fabienne",
+        userName: "FabienneUser",
+        password: "550143@CMA",
+        email: "fabi.teste@gmail.com",
+      })
+      .then((e) => e.data);
+  },
+  login: async (email, password) => {
+    return await server.post("user/login", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      email: email,
+      password: password,
+    });
+  },
 };
