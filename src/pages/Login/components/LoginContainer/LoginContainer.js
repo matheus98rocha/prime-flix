ec70dfff416d80f22659d24f21ba83dfc9675bd7
matheus98rocha <matheus98rocha@gmail.com>
2023-04-07 19:31:32 -0300

    feat - navigation login

--- src/pages/Login/components/LoginContainer/LoginContainer.js
+++ src/pages/Login/components/LoginContainer/LoginContainer.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DefaultInput from "../../../../componentes/DefaultInput/DefaultInput";
 import PrimaryButton from "../../../../componentes/PrimaryButton/PrimaryButton";
 import { LoginContainerWrapper } from "./loginContainer.styles";
 
 import { FormProvider, useForm } from "react-hook-form";
 import { userServices } from "../../../../services/api";
+import Loading from "../../../../componentes/Loading/Loading";
+import { useNavigate } from "react-router-dom";
 
 function LoginContainer() {
   const methods = useForm();
-  const { register, handleSubmit } = methods;
+  const { register, handleSubmit, formState } = methods;
+  const { isSubmitting } = formState;
+  const navigate = useNavigate();
 
   const onSubmit = (data) => {
-    userServices.login(data.email,data.password).then(() => {
-      alert("Logado com sucesso") 
-    }).catch(e => alert(e.message));
+    userServices
+      .login(data.email, data.password)
+      .then(() => {
+        navigate("/movies");
+      })
+      .catch((e) => alert(e.message));
   };
+
+    //Verify if the data is loading
+    if (isSubmitting) return <Loading />;
 
   return (
     <FormProvider {...methods}>
