0f7450b4c036590b5ac0c6b759dcd1df0a73c997
matheus98rocha <matheus98rocha@gmail.com>
2023-05-16 20:35:40 -0300

    feat - build realease

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import BackgroudImageLogin from "../../assets/backgroud-login-image.png";
 
 import * as S from "./Auth.styles";
